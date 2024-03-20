b6433f4ee1c0cd841f8c1909b81a2ecc99de6d0c
ash-hashtag <hashtag438@gmail.com>
2024-03-20 16:40:00 +0530

    added direct fan boost setting

--- src/main.zig
+++ src/main.zig
@@ -19,8 +19,8 @@ const Coordinates = struct {
 
 const TempFanGraph: [7]Coordinates = .{
     Coordinates{ .x = 0, .y = 0 },
-    Coordinates{ .x = 45, .y = 0 },
-    Coordinates{ .x = 46, .y = 48 },
+    Coordinates{ .x = 47, .y = 0 },
+    Coordinates{ .x = 48, .y = 38 },
     Coordinates{ .x = 50, .y = 64 },
     Coordinates{ .x = 55, .y = 100 },
     Coordinates{ .x = 60, .y = 128 },
@@ -69,15 +69,28 @@ pub fn main() !void {
         exit_flag.store(true, std.atomic.Ordering.SeqCst);
 
         thread.join();
+    } else if (std.mem.eql(u8, command, "fans")) {
+        var bufferWriter = std.io.bufferedWriter(std.io.getStdOut().writer());
+        const w = bufferWriter.writer();
+        const arg = args.next();
+        try w.print("{s}\n", .{CPU_ALIEN_DEVICE.name});
+        _ = try print_fan_info(CPU_ALIEN_DEVICE.fan_id, &w);
+        try w.print("{s}\n", .{GPU_ALIEN_DEVICE.name});
+        _ = try print_fan_info(GPU_ALIEN_DEVICE.fan_id, &w);
+        if (arg != null) {
+            const boost = try std.fmt.parseInt(u8, arg.?, 10);
+            try set_fan_boosts(boost, &w);
+        }
+
+        try bufferWriter.flush();
     } else if (std.mem.eql(u8, command, "mode")) {
         const arg = args.next();
         const mode = try get_power_mode();
         var bufferWriter = std.io.bufferedWriter(std.io.getStdOut().writer());
-        var w = bufferWriter.writer();
-        if (arg == null) {
-            const readableMode = if (mode == 0xab) "gmode" else "normal";
-            try w.print("Current Power Mode: {s}\n", .{readableMode});
-        } else {
+        const w = bufferWriter.writer();
+        const readableMode = if (mode == 0xab) "gmode" else "normal";
+        try w.print("Current Power Mode: {s}\n", .{readableMode});
+        if (arg != null) {
             const set_mode_to = arg.?;
             if (std.mem.eql(u8, set_mode_to, "gmode")) {
                 const result = try enable_gmode();
@@ -101,18 +114,25 @@ pub fn print_usage() !void {
     try w.print("Usage: \n", .{});
     try w.print("\tawc [COMMAND] [ARGUMENT] \n", .{});
     try w.print("COMMANDS: \n", .{});
-    try w.print("\t mode [gmode | normal]       - switch to gmode or normal \n", .{});
+    try w.print("\t mode [gmode | normal | ]    - switch to gmode or normal, or prints current mode if no mode is passed \n", .{});
     try w.print("\t watch                       - watch sensors and control fans automatically, the configuration of the relation between temparature and fan boost can be updated in main.zig\n", .{});
+    try w.print("\t fans [boost | ]             - set all fan boosts, or prints current fan boosts, if no boost value passed\n", .{});
 
     try buffer.flush();
 }
 
+pub fn print_fan_info(fan_id: u8, w: anytype) anyerror!u8 {
+    const boost = try get_fan_boost(fan_id);
+    const rpm = try get_fan_rpm(fan_id);
+    try w.print("\tBoost {s}{}{s}\n", .{ YELLOW, boost, RESET });
+    try w.print("\tRPM   {s}{}{s}\n", .{ GREEN, rpm, RESET });
+    return boost;
+}
+
 pub fn watch(exit_flag: *std.atomic.Atomic(bool)) anyerror!void {
     const power_mode = try get_power_mode();
-    var stdout = std.io.getStdOut();
-    var stdoutw = stdout.writer();
-    var buf = std.io.bufferedWriter(stdoutw);
-    var w = buf.writer();
+    var buf = std.io.bufferedWriter(std.io.getStdOut().writer());
+    const w = buf.writer();
     try w.print("Power Mode: {}\n", .{power_mode});
     if (0 != power_mode) {
         return error.PowerModeIsEnabled;
@@ -127,8 +147,8 @@ pub fn watch(exit_flag: *std.atomic.Atomic(bool)) anyerror!void {
         while (true) {
             std.time.sleep(20 * std.time.ns_per_ms);
             if (exit_flag.load(std.atomic.Ordering.SeqCst)) {
-                _ = try set_fan_boost(CPU_ALIEN_DEVICE.fan_id, 0);
-                _ = try set_fan_boost(GPU_ALIEN_DEVICE.fan_id, 0);
+                try set_fan_boosts(0, &w);
+                try buf.flush();
                 return;
             }
             const now = try std.time.Instant.now();
@@ -139,8 +159,25 @@ pub fn watch(exit_flag: *std.atomic.Atomic(bool)) anyerror!void {
     }
 }
 
+fn set_fan_boost_and_print(fan_id: u8, boost: u8, writer: anytype) anyerror!void {
+    const result = try set_fan_boost(fan_id, boost);
+    try writer.print("\tSet Fan Boost To {s}{}{s} result {s}{}{s}\n", .{
+        BOLD,
+        boost,
+        RESET,
+        CYAN,
+        result,
+        RESET,
+    });
+}
+
+fn set_fan_boosts(boost: u8, writer: anytype) anyerror!void {
+    try set_fan_boost_and_print(CPU_ALIEN_DEVICE.fan_id, boost, writer);
+    try set_fan_boost_and_print(GPU_ALIEN_DEVICE.fan_id, boost, writer);
+}
+
 fn update_device(device: *const AlienDevInfo, writer: anytype) anyerror!void {
-    var w = writer;
+    const w = writer;
     const temp = try get_temperature(device.sen_id);
     const timestamp = std.time.milliTimestamp();
     try w.print("{} Device {s}\n", .{ timestamp, device.name });
@@ -148,27 +185,27 @@ fn update_device(device: *const AlienDevInfo, writer: anytype) anyerror!void {
     for (TempFanGraph) |coord| {
         if (temp < coord.x) {
             const fan_boost = coord.y;
-            const current_fan_boost = try get_fan_boost(device.fan_id);
-            const current_fan_rpm = try get_fan_rpm(device.fan_id);
-            try w.print("\tBoost {s}{}{s}\n", .{ YELLOW, current_fan_boost, RESET });
-            try w.print("\tRPM   {s}{}{s}\n", .{ GREEN, current_fan_rpm, RESET });
+            const current_fan_boost = try print_fan_info(device.fan_id, w);
+            // const current_fan_boost = try get_fan_boost(device.fan_id);
+            // const current_fan_rpm = try get_fan_rpm(device.fan_id);
+            // try w.print("\tBoost {s}{}{s}\n", .{ YELLOW, current_fan_boost, RESET });
+            // try w.print("\tRPM   {s}{}{s}\n", .{ GREEN, current_fan_rpm, RESET });
             if (current_fan_boost != fan_boost) {
-                const result = try set_fan_boost(device.fan_id, fan_boost);
-                try w.print("\tSet Fan Boost To {s}{}{s} result {s}{}{s}\n", .{
-                    BOLD,
-                    fan_boost,
-                    RESET,
-                    CYAN,
-                    result,
-                    RESET,
-                });
+                try set_fan_boost_and_print(device.fan_id, fan_boost, w);
+                // const result = try set_fan_boost(device.fan_id, fan_boost);
+                // try w.print("\tSet Fan Boost To {s}{}{s} result {s}{}{s}\n", .{
+                //     BOLD,
+                //     fan_boost,
+                //     RESET,
+                //     CYAN,
+                //     result,
+                //     RESET,
+                // });
             }
             break;
         }
     }
 }
-
-fn show_info() !void {}
 
 fn get_fan_rpm(fan_id: u8) anyerror!i64 {
     return try run_main_command(0x14, 5, fan_id, 0);
